11419c2aebe627cf47b228e5e81ee4573067a7a1
Alexey Sedov <ASedov15@gmail.com>
2023-04-30 13:12:10 +0300

    Updates

---
+++ precache-manifest.ece4f7fcd51042a58041732cf3b6a499.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "74a60422efda9dad68d8",
+    "url": "/comment_list/static/js/main.9fa36d85.chunk.js"
+  },
+  {
+    "revision": "4f2d8a209260e248bb87",
+    "url": "/comment_list/static/js/runtime~main.f6670c94.js"
+  },
+  {
+    "revision": "3845d13312efd7b12742",
+    "url": "/comment_list/static/js/2.5c70deb8.chunk.js"
+  },
+  {
+    "revision": "d25c4dec5a7b517142b969aa0b522ab8",
+    "url": "/comment_list/static/media/ninja.d25c4dec.png"
+  },
+  {
+    "revision": "462e90b8fcbd77b77b5dcfaece68feae",
+    "url": "/comment_list/static/media/woman5.462e90b8.png"
+  },
+  {
+    "revision": "9a006d48a8e3f702aa3a6ab6ac5963d8",
+    "url": "/comment_list/index.html"
+  }
+];
